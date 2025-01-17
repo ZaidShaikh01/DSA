83a18ea9bc132ae873f3b877830752cf307ab673
ZaidShaikh <szaid5146@gmail.com>
2025-01-17 06:50:42 +0530

    Optimised Bubble Sort

--- Array/BubbleSort.java
+++ Array/BubbleSort.java
@@ -2,13 +2,20 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void bubbleSort(int [] arr){
+
         for (int i= arr.length-1;i>=0;i--){
+            int didSwap=0;
             for (int j=0;j<i;j++){
                 if (arr[j+1]<arr[j]){
                     int temp=arr[j+1];
                     arr[j+1]=arr[j];
                     arr[j]=temp;
+                    didSwap=1;
                 }
+
+            }
+            if (didSwap==0){
+                break;
             }
         }
     }
