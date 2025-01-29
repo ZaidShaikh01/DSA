859dfbcc3689ead37bf582a2ebf79044ee135656
ZaidShaikh <szaid5146@gmail.com>
2025-01-29 15:20:06 +0530

    Printing the maximum Sum Sub Array

--- Array/maximumSubArray.java
+++ Array/maximumSubArray.java
@@ -3,20 +3,37 @@ public class maximumSubArray {
     public static int maxSumOfSubArray(int []arr){
         int maximum=Integer.MIN_VALUE;
         int sum=0;
+        int ansStart=-1;
+        int ansEnd=-1;
+        int start=0;
 
         for (int i=0;i<arr.length;i++){
+            if(sum==0){
+                start=i;
+            }
             sum+=arr[i];
-            maximum=Integer.max(maximum,sum);
+
+            if (maximum<sum){
+
+                maximum=sum;
+                ansStart=start;
+                ansEnd=i;
+            }
             if (sum<0) sum=0;
 
         }
+        System.out.println("Now printing the sub array that gives maximum sum: ");
+        for (int i=ansStart;i<=ansEnd;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
 
 
         return maximum;
 
     }
     public static void main(String[] args) {
-        int [] arr={-1};
-        System.out.println(maxSumOfSubArray(arr));
+        int [] arr={-2,-3,4,-1,-2,1,5,-3};
+        System.out.println("Maximum Sum: "+maxSumOfSubArray(arr));
     }
 }
