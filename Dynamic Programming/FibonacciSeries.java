297b8ccbbc2e5f6eb86f965a63296da5b1ff391f
ZaidShaikh <szaid5146@gmail.com>
2025-03-18 08:22:30 +0530

    Fib with O(1) space

--- Dynamic Programming/FibonacciSeries.java
+++ Dynamic Programming/FibonacciSeries.java
@@ -13,8 +13,23 @@ public class FibonacciSeries {
 
         return arr[n]=fib(n-1,arr) + fib(n-2,arr);
 
+    }
+
+    public static int tabFib(int n){
+        int prev = 1;
+        int prev2 = 0;
+        int solution=0;
+
+        for (int i=2;i<=n;i++){
+            solution = prev + prev2;
+            prev2 =prev;
+            prev = solution;
+
+        }
+        return solution;
 
     }
+
     public static void main(String[] args) {
         int n=7;
         int [] arr=new int[n+1];
@@ -24,5 +39,6 @@ public class FibonacciSeries {
         System.out.println(fib(n,arr));;
 
         System.out.println(Arrays.toString(arr));
+        System.out.println(tabFib(8));
     }
 }
