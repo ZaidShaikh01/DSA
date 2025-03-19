d559ee6b78984809b3969c6bb4ebe7f5609ef28f
ZaidShaikh <szaid5146@gmail.com>
2025-03-19 12:44:28 +0530

    Recursive apparoch to memoization apparoch of MinimumCostToReachTop problem

--- Dynamic Programming/MinimumCostToReachTopFloor.java
+++ Dynamic Programming/MinimumCostToReachTopFloor.java
@@ -1,24 +1,39 @@
+import java.util.Arrays;
+
 public class MinimumCostToReachTopFloor {
 
     public static int minCostClimbingStairs(int[] cost) {
         // First of all there are a lot of choices and I can see that backtracking is used So, Maybe lets start with backtracking
         // Or Better I will try just with recursion but the choices are made so...
+
+
         int n = cost.length;
-        return Math.min(minCost(cost,n-1), minCost(cost,n-2));
+        int [] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+
+
+
+        return Math.min(minCost(cost,n-1,dp), minCost(cost,n-2,dp));
 
     }
-    public static int minCost(int [] cost, int step){
+    public static int minCost(int [] cost, int step, int [] dp){
         // Base case
         if(step<0){
             return 0;
         }
         // Second one
         if (step == 1 || step == 0) return cost[step];
-        return cost[step]+Math.min(minCost(cost,step - 1),minCost(cost,step-2));
+        // Cheking the dp array
+        if (dp[step]!=-1){
+            return dp[step];
+        }
+
+        return dp[step]=cost[step]+Math.min(minCost(cost,step - 1,dp),minCost(cost,step-2,dp));
     }
 
     public static void main(String[] args) {
         int [] cost ={1,100,1,1,1,100,1,1,100,1};
+
         System.out.println(minCostClimbingStairs(cost));
     }
 }
