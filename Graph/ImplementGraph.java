584430f7f983fa6dbc123d38167ddb8090f60faa
ZaidShaikh <szaid5146@gmail.com>
2025-07-23 13:21:01 +0530

    Implement Graph

--- Graph/ImplementGraph.java
+++ Graph/ImplementGraph.java
@@ -15,7 +15,7 @@ public class ImplementGraph {
         addVertex(u);
         addVertex(v);
         hm.get(u).add(v);
-        hm.get(v).add(u);
+        //hm.get(v).add(u); -- UNI DIRECTIONAL
     }
     void display(){
         for(int vertex : hm.keySet()){
@@ -26,7 +26,49 @@ public class ImplementGraph {
             System.out.println();
         }
     }
+//    public void dfs(int start){
+//        HashSet<Integer> visited = new HashSet();
+//        dfsHelper( start,visited);
+//    }
+//    public void dfsHelper(int curr,HashSet<Integer> visited ){
+//        visited.add(curr);
+//        System.out.print(curr + " ");
+//        for(int nbr : hm.get(curr)){
+//            if(!visited.contains(nbr))
+//                dfsHelper(nbr,visited);
+//        }
+//    }
+    public void dfs(int start){
+        HashSet<Integer> visited = new HashSet();
+        dfsHelper(start,visited);
+    }
+    public void dfsHelper(int start, HashSet<Integer> visited){
+        visited.add(start);
+        System.out.print(start+" ");
+        for(int neighbour: hm.get(start)){
+            if(!visited.contains(neighbour)){
+                dfsHelper(neighbour,visited);
+            }
+        }
+    }
+    public  boolean pathFind(int start, int end){
+        // base check if endpoint exist in the linkedlist of startpoint
+        // check for common elements between both start and end
+        HashSet<Integer> visited = new HashSet();
+        return pathFindRecurse(start,end,visited);
 
+    }
+    public boolean pathFindRecurse(int start,int end,HashSet<Integer> visited){
+        if(hm.get(start).contains(end)) return true;
+        visited.add(start);
+        //System.out.print(start + " ");
+        for(int neighbour: hm.get(start)){
+            if(!visited.contains(neighbour)){
+                pathFindRecurse(neighbour,end,visited);
+            }
+        }
+        return false;
+    }
     public static void main(String [] args) {
         ImplementGraph m = new ImplementGraph();
         Scanner sc = new Scanner(System.in);
@@ -43,6 +85,15 @@ public class ImplementGraph {
 
         System.out.println("\nGraph Representation:");
         m.display();
+        m.dfs(1);
+        //add(2,3,4,5,5,6,2,1,23);
+        System.out.print(m.pathFind(1,5));
     }
-
+    public static void add(int ...a ){
+        int sum = 0;
+        for (int i : a) {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
 }
